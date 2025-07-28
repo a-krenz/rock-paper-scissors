5b30ab888e437f3b42b328602b28be17c5b64086
Alexander Krenz <krenzalexander@proton.me>
2025-07-28 17:14:32 +0200

    feat: add function that returns random computer choice

---
+++ script.js
@@ -0,0 +1,17 @@
+function getComputerChoice() {
+    const rnd = Math.floor(Math.random() * 3)
+
+    switch (rnd) {
+        case 0:
+            return "rock";
+            break;
+        case 1:
+            return "paper";
+            break;
+        case 2:
+            return "scissors";
+            break;
+        default:
+            return "rock";
+    }
+}
